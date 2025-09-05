bb10ce2f5e208195354fcf7196bbd43c6f362420
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-09-05 22:37:50 +0600

    bare bones controllers

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Film {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDateTime releaseDate;
+    private float duration;
+}
