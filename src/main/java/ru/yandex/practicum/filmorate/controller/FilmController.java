b0742e005f8af6500f4a4dad957a486420977f16
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-09-06 10:29:33 +0600

    endpoints

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,4 +1,38 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/api/v1/")
 public class FilmController {
+    private List<Film> films = new ArrayList<>();
+
+    @GetMapping("/films")
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    @PostMapping("/films")
+    public Film createFilm(@RequestBody Film film) {
+        films.add(film);
+        return film;
+    }
+
+    @PutMapping("/films")
+    public Film putFilm(@RequestBody Film film) {
+        for (int i = 0; i < films.size(); i++) {
+            if (films.get(i).equals(film)) {
+                films.set(i, film);
+                return film;
+            }
+        }
+        films.add(film);
+        return film;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,4 +1,36 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
 public class UserController {
+    private List<User> users = new ArrayList<>();
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return users;
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        users.add(user);
+        return user;
+    }
+
+    @PutMapping("/users")
+    public User putUser(@RequestBody User user) {
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).equals(user)) {
+                users.set(i, user);
+                return user;
+            }
+        }
+        users.add(user);
+        return user;
+    }
 }
