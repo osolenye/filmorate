524aa95b2bb332dc2157287998c49084da8efc13
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-09-06 13:07:57 +0600

    logging

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -7,9 +9,10 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 
-
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/")
 public class FilmController {
@@ -22,43 +25,43 @@ public class FilmController {
 
     @PostMapping("/films")
     public Film createFilm(@RequestBody Film film) throws Exception{
-        if (film.getName() == "" || film.getName().equals(null)) {
-            throw new ValidationException("Film name can't be empty or blank.");
-        }
-        if (film.getDescription().length() > 200) {
-            throw new ValidationException("Film description can't be more than 200 characters.");
-        }
-        if (film.getReleaseDate().isBefore(LocalDateTime.of(1895, 12, 28, 0,0))) {
-            throw new ValidationException("Film's date can't be earlier than 28-12-1895");
-        }
-        if (film.getDuration() < 0) {
-            throw new ValidationException("Film's duration should be positive!");
-        }
+        validateFilm(film);
         films.add(film);
+        log.info("a film was created");
         return film;
     }
 
     @PutMapping("/films")
     public Film putFilm(@RequestBody Film film) throws Exception{
-        if (film.getName() == "" || film.getName().equals(null)) {
-            throw new ValidationException("Film name can't be empty or blank.");
-        }
-        if (film.getDescription().length() > 200) {
-            throw new ValidationException("Film description can't be more than 200 characters.");
-        }
-        if (film.getReleaseDate().isBefore(LocalDateTime.of(1895, 12, 28, 0,0))) {
-            throw new ValidationException("Film's date can't be earlier than 28-12-1895");
-        }
-        if (film.getDuration() < 0) {
-            throw new ValidationException("Film's duration should be positive!");
-        }
+        validateFilm(film);
         for (int i = 0; i < films.size(); i++) {
             if (films.get(i).equals(film)) {
                 films.set(i, film);
+                log.info("a film was changed");
                 return film;
             }
         }
         films.add(film);
+        log.info("a film was created");
         return film;
+    }
+
+    public void validateFilm(Film film) throws Exception{
+        if (film.getName() == "" || film.getName().equals(null)) {
+            log.warn("Validation exception");
+            throw new ValidationException("Film name can't be empty or blank.");
+        }
+        if (film.getDescription().length() > 200) {
+            log.warn("Validation exception");
+            throw new ValidationException("Film description can't be more than 200 characters.");
+        }
+        if (film.getReleaseDate().isBefore(LocalDateTime.of(1895, 12, 28, 0,0))) {
+            log.warn("Validation exception");
+            throw new ValidationException("Film's date can't be earlier than 28-12-1895");
+        }
+        if (film.getDuration() < 0) {
+            log.warn("Validation exception");
+            throw new ValidationException("Film's duration should be positive!");
+        }
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/")
 public class UserController {
@@ -21,45 +23,43 @@ public class UserController {
 
     @PostMapping("/users")
     public User createUser(@RequestBody User user) throws Exception {
-        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
-            throw new ValidationException("Email can't be empty and must contain '@'.");
-        }
-        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
-            throw new ValidationException("Login can't be empty and must not contain spaces.");
-        }
-        if (user.getReleaseDate() != null && user.getReleaseDate().isAfter(LocalDateTime.now())) {
-            throw new ValidationException("Birthday can't be in the future.");
-        }
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
-
+        validateUser(user);
         users.add(user);
+        log.info("A user has been created");
         return user;
     }
 
     @PutMapping("/users")
     public User putUser(@RequestBody User user) throws Exception {
-        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
-            throw new ValidationException("Email can't be empty and must contain '@'.");
-        }
-        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
-            throw new ValidationException("Login can't be empty and must not contain spaces.");
-        }
-        if (user.getReleaseDate() != null && user.getReleaseDate().isAfter(LocalDateTime.now())) {
-            throw new ValidationException("Birthday can't be in the future.");
-        }
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
-
+        validateUser(user);
         for (int i = 0; i < users.size(); i++) {
             if (users.get(i).equals(user)) {
                 users.set(i, user);
+                log.info("A user has been changed");
                 return user;
             }
         }
         users.add(user);
+        log.info("A user has been created");
         return user;
+    }
+
+    public void validateUser(User user) throws Exception {
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            log.warn("Validate exception");
+            throw new ValidationException("Email can't be empty and must contain '@'.");
+        }
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            log.warn("Validate exception");
+            throw new ValidationException("Login can't be empty and must not contain spaces.");
+        }
+        if (user.getReleaseDate() != null && user.getReleaseDate().isAfter(LocalDateTime.now())) {
+            log.warn("Validate exception");
+            throw new ValidationException("Birthday can't be in the future.");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            log.warn("Validate exception");
+            user.setName(user.getLogin());
+        }
     }
 }
